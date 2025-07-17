c3a53cda0cf39b73a1ef8b5f8b1b95984294dd9a
HX <122723004+Cantro93@users.noreply.github.com>
2025-07-17 18:01:21 +0200

    Update botcheck.js - licensing and comments

--- botcheck.js
+++ botcheck.js
@@ -1,3 +1,21 @@
+/*
+BotCheck
+    Copyright (C) 2025 Antoni Soltys
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 class BotCheck
 {
   constructor(time, css)
@@ -25,6 +43,7 @@ class BotCheck
     if (this.connections.has(ip) && (this.connections.has(ip)?.time != undefined && this.connections.has(ip)?.time != null ? this.connections.has(ip).time : 0)+this.timing < Date.now()) {
       // record exists and timeout has not occurred
       if (this.connections.get(ip).state == "pending") {
+        // record required evaluation, now it will be done
         let sess = this.connections.get(ip);
         if (data.includes(`${sess.id}=${sess.gr}`)) {
           sess.state = "accept";
