8f734244f117c41aba32d57aef7bfa9c1098e0bf
HX <122723004+Cantro93@users.noreply.github.com>
2025-07-17 18:24:15 +0200

    Update botcheck.js - licensing and comments

--- botcheck.js
+++ botcheck.js
@@ -1,19 +1,21 @@
 /*
 BotCheck
-    Copyright (C) 2025 Antoni Soltys
+    Copyright (C) 2025  Antoni Soltys
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
 */
 
 class BotCheck
@@ -21,9 +23,12 @@ class BotCheck
   constructor(time, css)
   {
     this.connections = new Map();
+    // css should use only selectors using element tags or input's type attribute - everything else is randomized
     this.css = css;
+    // as time is compared in milliseconds, conversion from seconds is required
     this.timing = time*1000;
   }
+  //generate new session entry
   mkSession() {
     return {
       id: this.mkRandom(),
@@ -33,6 +38,7 @@ class BotCheck
       time: Date.now()
     };
   }
+  // generate random hex strings
   mkRandom()
   {
     return Math.random().toString(16).substring(2);
